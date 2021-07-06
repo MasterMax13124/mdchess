1b5bd72b6af33e1f8b3fc93e339a0cf2b1e60ccf
iamlambda <imlambda@protonmail.com>
2021-07-06 22:52:04 +0200

    outline

--- chess.py
+++
@@ -1,9 +0,0 @@
-emptyrow = [".", ".", ".", ".", ".", ".", ".", "."]
-Pawnrow = ["P","P","P","P","P","P","P","P"]
-pawnrow = ["p","p","p","p","p","p","p","p"]
-Mainrow = ["R", "N", "B", "Q", "K", "B", "N", "R"]
-mainrow = ["r", "n", "b", "q", "k", "b", "n", "r"]
-board = [mainrow,pawnrow,emptyrow,emptyrow,emptyrow,emptyrow,pawnrow,mainrow]
-
-for i in board:
-    print("".join(i))
--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
+def printBoard(board : list):
+    for row in board:
+        for square in row:
+            print(square, end = ' ')
+        print()
+
+def parse_input() -> list:
+    # to complete, will return coordinates
+    pass
+
+
 def main():
-    return "max is beautiful"
+    board = [
+        ["r", "n", "b", "q", "k", "b", "n", "r"],
+        ["p" ,"p" ,"p", "p", "p", "p", "p", "p"],
+        [".", ".",".", ".", ".", ".", ".", "."],
+        [".", ".",".", ".", ".", ".", ".", "."],
+        [".", ".",".", ".", ".", ".", ".", "."],
+        [".", ".",".", ".", ".", ".", ".", "."],
+        [".", ".",".", ".", ".", ".", ".", "."],
+        ["P", "P", "P", "P", "P", "P", "P", "P"],
+        ["R", "N", "B", "Q", "K", "B", "N", "R"],
+    ]
+    printBoard(board) 
+
+if __name__ == '__main__':
+    main()
