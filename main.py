d3b8a386b7339ceeb7eac9abc46dbbef69169770
MasterMax13124 <mastermax13124@gmail.com>
2021-07-06 23:24:31 +0200

    Now everything is how I want it

--- main.py
+++ main.py
@@ -2,24 +2,21 @@
 
 def printBoard(board : list):
     for row in board:
-        for square in row:
-            print(square, end = ' ')
-        print()
+        print(" ".join(row))
 
-def parse_input() -> list:
-    # to complete, will return coordinates
-    pass
+def parse_input() -> list[str]:
+    # to complete, will return coordinates in list, as ["piece name", "row", column"]
+    return []
 
 
 def main():
     board = [
         ["r", "n", "b", "q", "k", "b", "n", "r"],
         ["p" ,"p" ,"p", "p", "p", "p", "p", "p"],
-        [".", ".",".", ".", ".", ".", ".", "."],
-        [".", ".",".", ".", ".", ".", ".", "."],
-        [".", ".",".", ".", ".", ".", ".", "."],
-        [".", ".",".", ".", ".", ".", ".", "."],
-        [".", ".",".", ".", ".", ".", ".", "."],
+        ["."]*8,
+        ["."]*8,
+        ["."]*8,
+        ["."]*8,
         ["P", "P", "P", "P", "P", "P", "P", "P"],
         ["R", "N", "B", "Q", "K", "B", "N", "R"],
     ]
