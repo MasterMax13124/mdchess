e0a77a2f27dd39708a05cb7828bf01ccb75755b1
MasterMax13124 <mastermax13124@gmail.com>
2021-07-07 14:30:11 +0200

    Implemented changes from main, hopefully automerge works now

--- pieceMovements.py
+++ pieceMovements.py
@@ -10,9 +10,16 @@ def replacement(board, player_move, old_position):
 def template(board, piece, o_row, o_column, moves, player):
     # player : 0 = white, 1 = black
     # return : -1 = move ain't possible, tuple (coordinates) = ain't no problemo bro
+
     for move in moves:
+        # doing this since we cant get static output from parse_input()
         row = o_row 
         column = o_column
+
+        # verifying that the goal square is free
+        if (board[row][column] in "PRNBQK" and player == 0 ) or (board[row][column] in "prnbqk" and player == 1):
+            return -1
+
         while (0 <= row + move[0] <= 7 ) and (0 <= column + move[1] <= 7):
             row += move[0]
             column += move[1]
@@ -21,7 +28,7 @@ def template(board, piece, o_row, o_column, moves, player):
                 continue
             elif (board[row][column] == f"{piece}" and player == 0) or (board[row][column] == f"{piece}".lower() and player == 1):
                 # piece found
-                return (row,column)
+                return (row,column) #returns the original position of the piece
             else:
                 # piece meet anything else
                 break
@@ -57,5 +64,18 @@ def queenMovements(board, player, player_move):
         [-1,-1],
         [-1,1],
         [1,-1]
+    ]
+    return template(board, player_move[0], player_move[1], player_move[2], moves, player)
+
+def knightMovements(board, player, player_move):
+    moves = [
+        [2,1],
+        [2,-1],
+        [-2,1],
+        [-2, -1],
+        [1, 2],
+        [1, -2],
+        [-1, 2],
+        [-1, -2],
     ]
     return template(board, player_move[0], player_move[1], player_move[2], moves, player)
